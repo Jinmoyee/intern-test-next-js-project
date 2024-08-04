465c8bef75ebfca9f76df527b2b81a2068bba958
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-08-04 22:27:34 +0530

    heelo

--- src/app/internships/page.jsx
+++ src/app/internships/page.jsx
@@ -5,7 +5,7 @@ export default function Internship() {
     return (
         <div className='flex'>
             <Sidebar />
-            <h1 className='border-l h-screen'>Internship</h1>
+            <h1 className='border-l min-h-screen pl-10 pt-7'>Internship</h1>
         </div>
     )
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,22 +1,29 @@
 import { CiFileOn } from "react-icons/ci";
 import { MdAutoGraph } from "react-icons/md";
 import { FaRegStarHalfStroke } from "react-icons/fa6";
+import Link from "next/link";
 
 export default function Sidebar() {
     return (
         <div className='mt-[5%] mr-4'>
-            <div className='flex items-center text-xl font-semibold text-gray-600 gap-3 px-12 py-5 rounded-r-full cursor-pointer hover:bg-gray-100'>
-                <MdAutoGraph size={30} />
-                <h3>Dashboard</h3>
-            </div>
-            <div className='flex items-center text-xl font-semibold text-gray-600 gap-3 px-12 py-5 rounded-r-full cursor-pointer hover:bg-gray-100 '>
-                <FaRegStarHalfStroke size={30} />
-                <h3>Skill Test</h3>
-            </div>
-            <div className='flex items-center text-xl font-semibold text-gray-600 gap-3 px-12 py-5 rounded-r-full cursor-pointer hover:bg-gray-100'>
-                <CiFileOn size={30} />
-                <h3>Internship</h3>
-            </div>
+            <Link href={'dashboard'}>
+                <div className='flex items-center text-xl font-semibold text-gray-600 gap-3 px-12 py-5 rounded-r-full cursor-pointer hover:bg-gray-100'>
+                    <MdAutoGraph size={30} />
+                    <h3>Dashboard</h3>
+                </div>
+            </Link>
+            <Link href={'skills'}>
+                <div className='flex items-center text-xl font-semibold text-gray-600 gap-3 px-12 py-5 rounded-r-full cursor-pointer hover:bg-gray-100 '>
+                    <FaRegStarHalfStroke size={30} />
+                    <h3>Skill Test</h3>
+                </div>
+            </Link>
+            <Link href={'internships'}>
+                <div className='flex items-center text-xl font-semibold text-gray-600 gap-3 px-12 py-5 rounded-r-full cursor-pointer hover:bg-gray-100'>
+                    <CiFileOn size={30} />
+                    <h3>Internship</h3>
+                </div>
+            </Link>
         </div>
     )
 }
